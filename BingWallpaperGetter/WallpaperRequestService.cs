2f4de389f37526dde483816e6621f03e592b0c7d
yzyDavid <yzyDavid@qq.com>
2016-04-23 13:25:56 +0800

    copied to page654.

--- BingWallpaperGetter/WallpaperRequestService.cs
+++ BingWallpaperGetter/WallpaperRequestService.cs
@@ -30,4 +30,60 @@ namespace BingWallpaperGetter
         {
         }
     }
+
+    public class ProgressEventArgs : EventArgs
+    {
+        public int ProgressValue { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public bool IsException { get; set; }
+
+        public string ExceptionInfo { get; set; }
+
+        public List<PictureInfo> Pictures { get; set; } 
+    }
+
+    /// <summary>
+    /// a tool class for get wallpaper info.
+    /// it is designed as singleton in the book.
+    /// </summary>
+    public static class WallpaperService
+    {
+        private static int SelectedDay { get; set; }
+
+        private static List<string> Countries { get; set; }
+        = new List<string>(new string[]
+        {
+            "zh-CN",
+            "fr-FR",
+            "de-DE",
+            "en-US",
+            "ja-JP",
+            "en-GB"
+        });
+
+        private static uint HttpTimesToRequest { get; set; }
+
+        private static uint HttpTimesTotal { get; set; }
+
+        private static bool IsDownloading { get; set; } = false;
+
+        //a list in dictionary? why?
+        private static Dictionary<uint, List<PictureInfo>> AllDownloadedPictures { get; set; }
+
+        public static EventHandler<ProgressEventArgs> GetOneDayWallpapersProgressEventHandler { get; set; }
+
+        private static void OnGetOneDayWallpapersProgressEvent(ProgressEventArgs progressEventArgs)
+        {
+            //WARNING not knowing if the sender is null will work.
+            GetOneDayWallpapersProgressEventHandler?.Invoke(null ,progressEventArgs);
+        }
+
+        //staic class CAN have a ctor?
+        static WallpaperService()
+        {
+            AllDownloadedPictures = new Dictionary<uint, List<PictureInfo>>();
+        }
+    }
 }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 A demo in the Windows 10 Universal APP Developing.
 Not finished yet.
+
+Aiming to learn UWP developing via this project.
